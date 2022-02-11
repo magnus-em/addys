400dd85ef67961b1a2ec0450f1cc2f3dba3a8eb2
Magnus Melbourne <magnus@mbp.lan>
2022-02-10 20:56:14 -0800

    variable port

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ app.get('/createaddy', async (req,res) => {
     res.send(newA)
 })
 
-app.listen(4000,() => {
-    console.log('app listening on port 3000')
+const port = process.env.port || 3001
+app.listen(port,() => {
+    console.log('app listening on port' + port)
 })
