8827743e79db4505958516faae6f168651db5719
magnus <melbournemagnus@gmail.com>
2022-02-28 18:07:38 -0800

    changed from bcrypt to bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router();
 
