213afb0c68992379fd2f4b931de83ca73f75ef75
magnus <melbournemagnus@gmail.com>
2022-03-03 19:14:26 -0800

    added seo descriptions

--- controllers/forwarder.js
+++ controllers/forwarder.js
@@ -3,5 +3,7 @@ module.exports.login = (req,res) => {
 }
 
 module.exports.landing = (req,res) => {
+    res.locals.title = 'Get Paid to Forward Packages From Your Home'
+    res.locals.description = 'Become a package forwarder and turn your home address into side income. Earn $15 dollars per package plus extras. Paid out weekly.'
     res.render('forwarder/landing')
 }
